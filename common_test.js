e02dbead3ca6bf04a59f7596d96266c1cedbe650
fuchengshuang <06125058@163.com>
2019-03-25 12:48:28 +0800

    5
    5

--- common_test.js
+++ common_test.js
@@ -15,11 +15,51 @@ if(pathName == "/module/agent/project_detail.html") {
 if(platformType > 0) {
 	var walesonc = 0;
 	var allcc = 0;
-	
-	function setBtnText(btn,text){
-		btn.value = text;
+	//本地时间和服务器 误差值
+	console.log("倒计时(毫秒):" + ProDet.reserObj.spareSec + ",服务器时间:" + ProDet.reserObj.localTime + "本地时间:" + Util.date.getDatetimes(null) + ",发布时间:" + ProDet.reserObj.releaseTime + ",预备时间(分):" + ProDet.reserObj.reserveTime);
+	var timeErrRange = Util.date.str2Date(ProDet.reserObj.localTime) - new Date().getTime() - 200;
+
+	console.log("误差时间:" + timeErrRange);
+
+	// 获取当前时间
+	getCurTime: function() {
+		return new Date().getTime() + timeErrRange;
 	}
-	
+
+	function setBtnText(btn, text) {
+		if(btn != null) {
+			btn.value = text;
+		}
+	}
+	/**
+	 * 判断还有多长时间可以开始倒计时
+	 * @param releaseTime 发布时间
+	 * @param reserveTime 预留时间
+	 */
+	exCompareTime: function(releaseTime, reserveTime) {
+		var spareSec = 0;
+		if(releaseTime && reserveTime) {
+			var relTime = Util.date.str2Date(releaseTime),
+				relSec = reserveTime * 60 * 1000;
+			spareSec = relSec - (getCurTime() - relTime);
+		}
+		return spareSec < 0 ? 0 : spareSec;
+	}
+
+	// 时间倒计时函数
+	exTimeCountDown: function() {
+		var totalrRmain = exCompareTime(ProDet.reserObj.releaseTime, ProDet.reserObj.reserveTime);
+		// 如果已经可以抢单(小于1秒抢单)
+		if(totalrRmain < 1000) {
+			placeOrder();
+		} else {
+			if(totalrRmain > 1000) {
+				setTimeout(exTimeCountDown, 1000);
+			}
+			setBtnText(document.getElementById("waleson_auto_click"), ProDet.secsToHMS(totalrRmain));
+		}
+	}
+
 	function Initlabel() {
 		console.log("Initlabel...");
 		start = sessionStorage.getItem("ClickStart");
@@ -35,25 +75,32 @@ if(platformType > 0) {
 		}
 	}
 
+	function placeOrder() {
+		sessionStorage.setItem("ClickStart", "true");
+		setBtnText(walesonAddBtn, "已开启自动抢单");
+	}
+
 	function StartAuto() {
 		walesonAddBtn = document.getElementById("waleson_auto_click");
 		if(walesonAddBtn != null) {
+			//自动定时抢单 1判断是否是倒计时抢单 算出服务器与本地误差时间
+
 			start = sessionStorage.getItem("ClickStart");
 			if(start == "true") {
 				sessionStorage.setItem("ClickStart", "false");
 				//walesonAddBtn.innerHTML = "已停止自动抢单";
-				setBtnText(walesonAddBtn,"已停止自动抢单");
+				setBtnText(walesonAddBtn, "已停止自动抢单");
 			} else {
 				ccc = prompt("请输入频率s", "0.2");
+				console.log("--ccc--" + ccc);
 				if(ccc != null) {
 					freq = ccc * 1000;
 					if(freq < 50) {
 						freq = 50
 					};
 					sessionStorage.setItem("freq", freq);
-					sessionStorage.setItem("ClickStart", "true");
-					setBtnText(walesonAddBtn,"已开启自动抢单");
 					//walesonAddBtn.innerHTML = "已开启自动抢单";
+					exTimeCountDown();
 				}
 			};
 		} else {
@@ -63,6 +110,7 @@ if(platformType > 0) {
 
 	function CheckClick() {
 		start = sessionStorage.getItem("ClickStart");
+		console.log("--CheckClick--" + start);
 		if(start == "true") {
 			mydate = new Date();
 			timeok = false;
@@ -75,17 +123,17 @@ if(platformType > 0) {
 					if(allcc >= freq) {
 						if($(".J_grab_single").hasClass("j-ishost")) {
 							//wbtn.innerHTML = "主项目自动抢单" + walesonc + "次";
-							setBtnText(wbtn,"主项目自动抢单" + walesonc + "次");
+							setBtnText(wbtn, "主项目自动抢单" + walesonc + "次");
 							if(platformType == 2) {
-								grabSingle(ProDet.busId, null, "isCsb");
+								//grabSingle(ProDet.busId, null, "isCsb");
 							} else {
-								grabSingle(ProDet.busId, "", "", true);
+								//grabSingle(ProDet.busId, "", "", true);
 							}
 
 						} else {
 							//wbtn.innerHTML = "子项目自动抢单" + walesonc + "次";
-							setBtnText(wbtn,"子项目自动抢单" + walesonc + "次");
-							grabSingle(ProDet.busId);
+							setBtnText(wbtn, "子项目自动抢单" + walesonc + "次");
+							//grabSingle(ProDet.busId);
 						}
 
 						walesonc += 1;
@@ -124,6 +172,11 @@ if(platformType > 0) {
 
 	function initAutoclick() {
 		console.log("------- start initAutoclick ---------");
+		var abc = sessionStorage.getItem("abc");
+		console.log("abc:" + abc);
+		if(abc == null) {
+			sessionStorage.setItem(abc, true)
+		}
 		var start = false;
 		var freq = 200;
 		freq = sessionStorage.getItem("freq");
@@ -133,8 +186,6 @@ if(platformType > 0) {
 		setInterval("CheckClick()", 100);
 		AddBtn();
 		Initlabel();
-		console.log("spareSec:"+ProDet.reserObj.spareSec+",localTime:"+ProDet.reserObj.localTime+",releaseTime:"+ProDet.reserObj.releaseTime+",ProDet.reserObj.reserveTime:"+ProDet.reserObj.reserveTime);
-		ProDet.btnIsEnable(ProDet.reserObj.$btn, false);
 	}
 
 	if(platformType == 2) {
